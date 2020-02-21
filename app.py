4ff52f9e03fc744bee93009d502be1eb46ae271a
lakshminarasimhan santhanam <sln2737@gmail.com>
2020-02-21 22:49:32 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -5,29 +5,32 @@ Spyder Editor
 This is a temporary script file.
 """
 from flask import Flask, request, jsonify, redirect, url_for, flash, Response
-
 import datetime as dt
 import numpy as np
 import ktrain as kt
 import json
 from gevent.pywsgi import WSGIServer
+from urllib.parse import *
 
 app = Flask(__name__)
 
 @app.route('/api/', methods=['POST'])
 def predict():
-    jdata = request.get_json()
-    data=json.dumps(jdata)
-    print(data)
-    results=predictor.predict(data)
-    class_prob=predictor.predict(data,return_proba=True).max()*100
-    class_text=predictor.predict(data)
-    print ('{0} - {1}%'.format(class_text,class_prob))
-    json_output=[class_text,class_prob]
-    return jsonify(json_output)
+    if 'TEXT' in request.args:
+        jdata = unquote(request.args['TEXT'])
+        data=json.dumps(jdata)
+        print(data)
+        class_prob=predictor.predict(data,return_proba=True).max()*100
+        class_text=predictor.predict(data)
+        print ('{0} - {1}%'.format(class_text,class_prob))
+        json_output=[class_text,class_prob]
+        return jsonify(json_output)
+    else:
+        return "Error: No TEXT field provided. Please contact admin for assistance."
+    
 
 if __name__ == '__main__':
-    model_folder = r'C:\Test\SWOT\Training\BERT\Saved_Model\saved_model_25k'
+    model_folder = r'.\run264_16\saved_model_25k'
     predictor = kt.load_predictor(model_folder)
     print(dt.datetime.now(), 'started..')
     app_server = WSGIServer(('127.0.0.1', 8088), app)
--- request.py
+++ request.py
@@ -10,8 +10,7 @@ import json
 
 url = 'http://127.0.0.1:8088/api/'
 
-data = ['Technology has journeyed from hardware to enterprise software to SMAC (social media, analytics and cloud) and artificial intelligence, while at the same time; it has become an integral part of every industry in an increasingly multi-device connected world. Smart machines, cognitive computing and internet of things are narrowing divide between humans and machines, and creating new applications in a world of cognizant computing where technology will take decisions for humans based on historical data and preferences. Thus, Information Technology is continuously evolving and the trend is expected to continue in future.']
-
+data = '{  "TEXT": ['Technology has journeyed from hardware to enterprise software to SMAC (social media, analytics and cloud) and artificial intelligence, while at the same time; it has become an integral part of every industry in an increasingly multi-device connected world. Smart machines, cognitive computing and internet of things are narrowing divide between humans and machines, and creating new applications in a world of cognizant computing where technology will take decisions for humans based on historical data and preferences. Thus, Information Technology is continuously evolving and the trend is expected to continue in future.'] }'
 j_data = json.dumps(data)
 headers = {'content-type': 'application/json', 'Accept-Charset': 'UTF-8'}
 c = requests.post(url, data=j_data, headers=headers)
